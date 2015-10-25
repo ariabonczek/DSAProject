7d49894f47bd38b22a2532e2fb2d73c8608c2172
unknown <jl4312@NM2550-03.main.ad.rit.edu>
2015-10-25 17:54:00 -0400

    Car class

---
+++ src/Car.cpp
@@ -0,0 +1,73 @@
+#include "Car.h"
+
+NS_BEGIN
+
+Car :: Car(std::string name, Mesh* mesh, Material* material,Transform* trans)
+	:GameObject(name, mesh, material)
+{
+
+	this->transform = trans;
+}
+
+Car::Car(const Car& object)
+	:GameObject(object)
+{
+	this->name = object.name;
+	this->mesh = object.mesh;
+	this->material = object.material;
+	this->transform = object.transform;
+}
+
+Car::~Car()
+{
+	
+}
+
+Car& Car::operator=(const Car& object)
+{
+	this->name = object.name;
+	this->mesh = object.mesh;
+	this->material = object.material;
+	this->transform = object.transform;
+
+	return *this;
+}
+
+void Car::Update(float dt)
+{
+	CalcForce();
+
+	velocity.Add(velocity, acceleration);
+
+	
+	if (CalcMagnitude(velocity) > maxSpeed)
+	{
+		velocity.SetMagnitude(maxSpeed);
+	}
+
+	transform->Translate(velocity);
+
+	acceleration = Vector3(0,0,0);
+}
+
+void Car::CalcForce()
+{
+	Vector3 force = Vector3(0.0f);
+
+	if (CalcMagnitude(force) > maxForce)
+	{
+		force.SetMagnitude(maxForce);
+	}
+	
+}
+
+void Car::ApplyForce(Vector3 force)
+{
+	acceleration.Add(acceleration, force);
+}
+
+float Car::CalcMagnitude(Vector3 force)
+{
+	return sqrt(force.x * force.x + force.y * force.y + force.z * force.z);
+}
+NS_END
---
+++ src/Car.h
@@ -0,0 +1,44 @@
+#pragma once
+#ifndef CAR_H
+#define CAR_H
+
+
+#include "Graphics/Transform.hpp"
+#include "Graphics/GameObject.hpp"
+
+NS_BEGIN
+class Car : public GameObject
+{
+	
+public:
+	Car(std::string name, Mesh* mesh, Material* material, Transform* trans);
+	Car(const Car& car);
+	~Car();
+	
+	Car& operator=(const Car& car);
+	
+	void Update(float dt);
+	void CalcForce();
+	float CalcMagnitude(Vector3 Force);
+	void ApplyForce(Vector3 force);
+private:
+	Vector3 velocity;
+	Vector3 acceleration;
+
+	const float MAX_SPEED = 5;
+	
+	std::string name;
+	Transform* transform;
+	Mesh* mesh;
+	Material* material;
+
+	int mass;
+
+	float maxSpeed;
+	float maxForce;
+
+
+};
+
+NS_END
+#endif
